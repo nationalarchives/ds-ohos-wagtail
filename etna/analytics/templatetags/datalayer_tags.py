9917ac261c17823f9c570dd44a3a1029ec39fa19
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-01-27 14:05:57 +0000

    Remove unused GTM datalayer rendering from templates

--- etna/analytics/templatetags/datalayer_tags.py
+++ etna/analytics/templatetags/datalayer_tags.py
@@ -3,21 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("includes/gtm-datalayer.html", takes_context=True)
-def render_gtm_datalayer(context, obj) -> dict:
-    """
-    Render the datalayer for an instance of a ``DataLayerMixin`` subclass.
-    https://developers.google.com/tag-manager/devguide
-    """
-    try:
-        data = obj.get_datalayer_data(context["request"])
-    except AttributeError:
-        data = {}
-
-    # Add a json serialized version of the data for output
-    return {"data": data}
-
-
 @register.simple_tag(takes_context=True)
 def image_browse_datalayer(context) -> dict:
     """Custom tag for image browse datalayer.
